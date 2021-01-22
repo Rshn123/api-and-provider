5a4483afb0132a3df276f8d861db5989923e4089
Roshan Shrestha <rshrestha0110@gmail.com>
2021-01-22 19:19:39 +0545

    Main screen card modified

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -15,34 +15,91 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       appBar: AppBar(title: Text("Api")),
       body: Container(
-          padding: EdgeInsets.all(20),
           child: FutureBuilder(
               future: userDatas.getUserData(context),
               builder: (BuildContext context,
                   AsyncSnapshot<List<UserData>> snapShot) {
                 return snapShot.data == null
-                    ? CircularProgressIndicator()
+                    ? Center(child: CircularProgressIndicator())
                     : ListView(
                         children: snapShot.data
-                            .map((UserData userData) => Card(
-                                    child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Column(
-                                      children: [
-                                        Text("Name: " + userData.name),
-                                      ],
-                                    ),
-                                    IconButton(
-                                      icon: Icon(Icons.delete),
-                                      onPressed: () async {
-                                        await userDatas.deleteUserData(
-                                            context, userData.id.toString());
-                                      },
-                                    )
-                                  ],
-                                )))
+                            .map((UserData userData) => Padding(
+                              padding: const EdgeInsets.fromLTRB(14,8,14,0),
+                              child: Card(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Row(
+                                    mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                    children: [
+                                        Row(
+                                          children: [
+                                            Container(
+                                              height: 55,
+                                              width: 55,
+                                              decoration: BoxDecoration(
+                                                border: Border.all(
+                                                    width: 4,
+                                                    color: Colors.white
+                                                        ),
+                                                shape: BoxShape.circle,
+                                                boxShadow: [
+                                                  BoxShadow(
+                                                    color:snapShot.data == null? Colors.white:Colors.grey
+                                                        .withOpacity(0.4),
+                                                    spreadRadius: 5,
+                                                    blurRadius: 4,
+                                                    offset: Offset(0,
+                                                        3), // changes position of shadow
+                                                  ),
+                                                ],
+                                              ),
+                                              child: Container(
+                                                height: 50,
+                                                width: 50,
+                                                decoration: BoxDecoration(
+                                                  border: Border.all(
+                                                      width: 1,
+                                                      color: Colors.white
+                                                          .withOpacity(0.9)),
+                                                  shape: BoxShape.circle,
+                                                  image: DecorationImage(
+                                                      image: AssetImage(
+                                                        "assets/images/pic${userData.id}"
+                                                        ".jpg",
+                                                      ),
+                                                      fit: BoxFit.cover),
+                                                  boxShadow: [
+                                                    BoxShadow(
+                                                      color:snapShot.data == null? Colors.white:Colors.grey
+                                                          .withOpacity(0.4),
+                                                      spreadRadius: 5,
+                                                      blurRadius: 4,
+                                                      offset: Offset(0,
+                                                          3), // changes position of shadow
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                            SizedBox(width: 10),
+                                            Text(userData.name,
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold)),
+                                          ],
+                                        ),
+                                        IconButton(
+                                          icon:
+                                              Icon(Icons.delete, color: Colors.red.withOpacity(0.7)),
+                                          onPressed: () async {
+                                            await userDatas.deleteUserData(
+                                                context, userData.id.toString());
+                                          },
+                                        )
+                                    ],
+                                  ),
+                                      )),
+                            ))
                             .toList(),
                       );
               })),
